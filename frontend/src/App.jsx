b15e8e357f72c56a27f5042d064a5ced17064d17
burakkepek <54933287+burakkepek@users.noreply.github.com>
2023-06-12 12:06:35 +0300

    Refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
-
 const App = () => {
-    return <Cart />
+  return <Cart />;
 };
 
 export default App;
